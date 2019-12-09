b58de0f0fcb8af6448ac6fc37af070ef89a2b8d5
Yingying Xiong <yxiong@dot.nyc.gov>
2019-12-09 15:12:06 -0500

    small fix

--- src/bigbrothers/store/rootReducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import ticketReducer from "../containers/ticket/redux/ticketReducer";
-import projectReducer from "../containers/projects/redux/projectReducer";
-
-export default combineReducers({
-  ticket: ticketReducer,
-  project: projectReducer
-});
