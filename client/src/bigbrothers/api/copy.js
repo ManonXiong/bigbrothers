a680e2b62adf836a7670fe403287a28f620bd5b2
Yingying Xiong <yxiong@dot.nyc.gov>
2019-12-18 10:56:06 -0500

    small fix

--- client/src/bigbrothers/api/copy.js
+++ client/src/bigbrothers/api/copy.js
@@ -1,25 +1,25 @@
-componentDidMount() {
-  const startInd = this.PaymentResponse.location.pathname.indexOf("/mybn") + "/mybn/".length;
-  const endInd = this.PaymentResponse.location.pathname.indexOf("/mybn") + "/mybn/".length + 4;
-  this.PaymentResponse.setAdminPageStroeID(this.PaymentResponse.location.pathname.substring(startInd, endInd));
-  var holdTimeout = setInterval(
-    function() {
-      if (this.props.isURILoaded) {
-        console.log(`this.props.isURILoaded is loaded`);
-        clearInterval(holdTimeout);
-        const script = document.createElement("script");
-        script.scr = this.props.SCRIPT_URI;
-        script.async = true;
-        script.crossOrigin = true;
-        document.body.appendChild(script);
+// componentDidMount() {
+//   const startInd = this.PaymentResponse.location.pathname.indexOf("/mybn") + "/mybn/".length;
+//   const endInd = this.PaymentResponse.location.pathname.indexOf("/mybn") + "/mybn/".length + 4;
+//   this.PaymentResponse.setAdminPageStroeID(this.PaymentResponse.location.pathname.substring(startInd, endInd));
+//   var holdTimeout = setInterval(
+//     function() {
+//       if (this.props.isURILoaded) {
+//         console.log(`this.props.isURILoaded is loaded`);
+//         clearInterval(holdTimeout);
+//         const script = document.createElement("script");
+//         script.scr = this.props.SCRIPT_URI;
+//         script.async = true;
+//         script.crossOrigin = true;
+//         document.body.appendChild(script);
 
-        let hostUrl = this.props.HOST_URI;
-        let storeID = this.props.storeID;
-        this.props.loadPendingCards(hostUrl, storeID);
-      } else {
-          console.log(`this.props.isURILoaded is loading`);
-      }.bind(this),
-      1000
-    }
-  )
-}
+//         let hostUrl = this.props.HOST_URI;
+//         let storeID = this.props.storeID;
+//         this.props.loadPendingCards(hostUrl, storeID);
+//       } else {
+//           console.log(`this.props.isURILoaded is loading`);
+//       }.bind(this),
+//       1000
+//     }
+//   )
+// }
